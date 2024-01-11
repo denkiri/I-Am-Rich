978cf98dfdc11292340b2e3046d61ea3ba9727af
denkiri <denniskirimi.dk@gmail.com>
2024-01-11 10:08:04 +0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Macbook Pro on 11/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
